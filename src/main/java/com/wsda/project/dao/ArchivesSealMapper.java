290e8f3085953425515188e4680c2df400e72b57
2287004341@qq.com <2287004341@qq.com>
2019-07-09 09:44:07 +0800

    实体列字段名规范书写

--- src/main/java/com/wsda/project/dao/ArchivesSealMapper.java
+++ src/main/java/com/wsda/project/dao/ArchivesSealMapper.java
@@ -4,6 +4,9 @@ package com.wsda.project.dao;
 import com.wsda.project.model.ArchivesSeal;
 import org.apache.ibatis.annotations.Param;
 
+/**
+ * 归档章
+ */
 public interface ArchivesSealMapper {
     ArchivesSeal getArchivesSealByTableCode(@Param("tableCode") String tableCode);
     Integer delArchivesSeal(@Param("id")String id);
